9679cf2623170481a0173c874bf81856a03ed918
BT_1N3_22 <BT_1N3_22@BT_1N3_22.bktech.local>
2017-09-29 11:36:36 -0400

    vbcv

---
+++ DuelProjectFahad/src/chatbotproject/ChatbotTyler.java
@@ -0,0 +1,44 @@
+package chatbotproject;
+
+public class ChatbotTyler implements Topic {
+	private String[] keywords;
+	private String goodbyewords;
+	private String secretWord;
+	private boolean chatting;
+	
+	public ChatbotTyler() {
+		String[] temp = {"food","entertainment","internet","video games"};
+		keywords = temp;
+		String temp2 = "jeff";
+		goodbyewords = temp2;
+		secretWord = "doge";
+		
+	}
+	
+	public boolean isTriggered(String response) {
+		for (int i = 0; i < keywords.length; i++) {
+			if(ChatbotMain.findKeyword(response, keywords[i], 0)>= 0)
+				return true;
+		}
+		return false;	
+	}
+	
+	
+	public void startChatting(String response) {
+		ChatbotMain.print("Suh dude! It sounds like we have something in common!");
+		chatting = true;
+		while (chatting) {
+			 response = ChatbotMain.getInput();
+			if (ChatbotMain.findKeyword(response,goodbyewords, 0)>=0) {
+				chatting = false;
+				ChatbotMain.chatbot.startTalking();
+			}else if (ChatbotMain.findKeyword(response, secretWord, 0) >=0) {
+				ChatbotMain.print("Cool! You guessed my favorite thing ever. We are friends now.");		
+			}else {
+				
+				ChatbotMain.print("what");
+			}
+			
+		}
+	}
+}
