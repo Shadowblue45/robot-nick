55b32ab9efc834bcca706cc42b34cb0c619c6284
BT_1N3_33 <BT_1N3_33@BT_1N3_33.bktech.local>
2017-10-06 11:36:35 -0400

    i am z best

--- DuelProjectFahad/src/chatbotproject/ChatbotDavid.java
+++ DuelProjectFahad/src/chatbotproject/ChatbotDavid.java
@@ -11,12 +11,15 @@ public class ChatbotDavid implements Topic {
 	private boolean isMoba;
 	private boolean chatting;
 	private String[] unknownGames = {null, null, null, null};
-	private boolean wasLearned = false;
+	private boolean wasLearned1 = false;
+	private boolean wasLearned2 = false;
+	private boolean wasLearned3 = false;
+	private boolean wasLearned0 = false;
 	
 	public ChatbotDavid() {
-		String[] temp2 = {"LOL","Smite","Awesomenauts","Dota","Heros of the Storm"};
-		String[] temp3 = {"Minecraft","Terraria","Gmod","GTA","No Man's Sky"};
-		String[] temp = {"Mobas","moba","mobas","Moba","sandboxes","sand-boxes","sandbox","sand-box"};
+		String[] temp2 = {"lol","smite","awesomenauts","dota","heros of the storm","league of legends"};
+		String[] temp3 = {"minecraft","terraria","gmod","gta","no man's sky","garrys mod","garry's mod"};
+		String[] temp = {"mobas","moba","sandboxes","sand-boxes","sandbox","sand-box"};
 		keywords = temp;
 		Mobas = temp2;
 		Sandboxes = temp3;
@@ -27,7 +30,7 @@ public class ChatbotDavid implements Topic {
 	public boolean isTriggered(String response) {
 		for(int i = 0; i < keywords.length;i++) {
 			if(ChatbotMain.findKeyword(response, keywords[i], 0) >= 0) {
-				if(i > 3) {
+				if(i > 1) {
 					isSandbox = true;
 					isMoba = false;
 				}
@@ -50,23 +53,21 @@ public class ChatbotDavid implements Topic {
 		while(chatting) {
 			response = ChatbotMain.getInput();
 			String favoriteGame = response;
-			if(isSandbox)
-				sandBoxes(favoriteGame);
-			else if(isMoba)
-				mobas(favoriteGame);
-			else if(ChatbotMain.findKeyword(response, goodbyeWord, 0) >= 0) {
+			favoriteGame = favoriteGame.toLowerCase();
+			if(ChatbotMain.findKeyword(response, goodbyeWord, 0) >= 0) {
 				chatting = false;
 				ChatbotMain.chatbot.startTalking();
-			} else if(ChatbotMain.findKeyword(response, secretWord, 0) >= 0) {
+			} else if(ChatbotMain.findKeyword(response, secretWord, 0) >= 0) 
 				ChatbotMain.print("I'm just as confused?");
-			} else if(ChatbotMain.findKeyword(response, secretWord, 0) >= 0) {
-				
-				}else {
+			else if(isSandbox)
+				sandBoxes(favoriteGame);
+			else if(isMoba)
+				mobas(favoriteGame);	
+			else {
 				ChatbotMain.print("Wuh? Repeat please.");
 			}
-		}
 	}
-
+}
 
 	public void sandBoxes(String response) {
 			boolean wasFound = false;
@@ -97,22 +98,24 @@ public class ChatbotDavid implements Topic {
 						else
 							ChatbotMain.print("not nice");
 					*/	
-						if(wasLearned && unknownGames != null && unknownGames[j].contains(response)) 
+						if((wasLearned1 || wasLearned2 || wasLearned3 || wasLearned0) && unknownGames[j] != null && unknownGames[j].contains(response)) 
 						{
-							ChatbotMain.print("Thanks for teaching me about" + response);
+							ChatbotMain.print("Thanks for teaching me about " + response);
+							return;
 						}
 						
 						else if(unknownGames[j] == null) 
 						{
 							unknownGames[j] = response;
 							ChatbotMain.print("I've never heard of that game before.");
-							notWasLearned();
+							notWasLearned(j);
 							return;
 						}
 							
 					}
 				if(unknownGames[3] != null)
 					ChatbotMain.print("Sorry, I can't remember so many new games!");
+				return;
 		}
 	}
 
@@ -120,7 +123,14 @@ public class ChatbotDavid implements Topic {
 		
 	}
 	
-	private void notWasLearned() {
-		wasLearned = !wasLearned;
+	private void notWasLearned(int num) {
+		if(num == 0)
+			wasLearned0 = !wasLearned0;
+		if(num == 1)
+			wasLearned1 = !wasLearned1;
+		if(num == 0)
+			wasLearned2 = !wasLearned2;
+		if(num == 0)
+			wasLearned3 = !wasLearned3;
 	}
 }
